5a0c46b5806cf3fb6a9d1237353fe29f6c02990e
myriam graine <myriam.bdn@gmail.com>
2024-02-08 11:42:58 +0100

    adding routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def projets
+  end
+
+  def equipe
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'projets', to: 'pages#projets'
+  get 'equipe', to: 'pages#equipe'
+  get 'contact', to: 'pages#contact'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
 end
